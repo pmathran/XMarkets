21a76895f12c70c88d9e5a460ed8a9a70350b9f2
Sreejith Raman <sreejithraman@gmail.com>
2013-02-12 18:45:41 -0500

    Added files for Android
    take 1

---
+++ src/com/team/XMarkets/XMarketsActivity.java
@@ -0,0 +1,13 @@
+package com.team.XMarkets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class XMarketsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
